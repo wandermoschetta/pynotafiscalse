bf4187ca7efe8eb87668a818473f389d4a14b499
wandermoschetta <wander.sm@gmail.com>
2019-05-10 14:41:01 -0300

    Criado pacote da Nota Fiscal Eletronica. Close #2

---
+++ notafiscalse/__init__.py
@@ -0,0 +1,1 @@
+# Módulo da Nota Fiscal de Serviços Eletrônico 
---
+++ notafiscalse/ribeirao_preto_sp/__init__.py
@@ -0,0 +1,4 @@
+#  Módulo da Nota Fiscal de Serviços Eletrônico da Cidade de Ribeirão Preto-SP
+#  Nota Fiscal de Serviços Versão 3
+#  Documentação oficial:
+#  http://ribeiraopreto.ginfesh.com.br/
