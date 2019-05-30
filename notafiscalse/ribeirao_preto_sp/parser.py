187f312e898bfe0147a01adac3f12a1d1beb0127
wandermoschetta <wander.sm@gmail.com>
2019-05-30 14:52:36 -0300

    Ajuste pacote ribeirao_preto_sp

---
+++ notafiscalse/ribeirao_preto_sp/parser.py
@@ -0,0 +1,13 @@
+"""
+  Módulo responsável pelo tratamento dos dados para XML
+"""
+
+class SetupXml:
+  """
+    Classe responsável pelo tratamento dos dados para XML
+  """
+  def cabecalho_envia_lote_rps(self):
+    return '<ns2:cabecalho versao="3" xmlns:ns2="http://www.ginfes.com.br/cabecalho_v03.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><versaoDados>3</versaoDados></ns2:cabecalho>'
+              
+  def envelope_envia_lote_rps(self):
+    return '{}'.format(self.cabecalho_envia_lote_rps())
